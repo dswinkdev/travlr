f424fa81402e1ba42da97e87a9987ac130f45967
Derricko Swink <dswinkgfx@gmail.com>
2025-05-31 12:53:19 -0500

    Module 4 completed baseline

---
+++ app_server/models/travlr.js
@@ -0,0 +1,16 @@
+const moogoose = require('mongoose');
+const { index } = require('../controllers/main');
+
+// Define the trip schema
+const tripSchema = new moogoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: { type: String, required: true, index: true },
+    length: { type: String, required: true },
+    start: { type: Date, required: true },
+    resort: { type: String, required: true },
+    perPerson: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true }
+});
+const Trip = moogoose.model('trips', tripSchema);
+module.exports = Trip;
